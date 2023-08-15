323e008f3a1553c9addab8d1f85ed3885f72d1c6
Tom Gagliano <tgaglianoll@gmail.com>
2023-08-15 18:56:02 -0400

    solve reverse question in python

---
+++ array/reverse.py
@@ -0,0 +1,10 @@
+# Given an array, write a function that returns an array that contains the original array’s values in reverse.
+
+# Input: [1, 2, 3, 4, 5]
+# Output: [5, 4, 3, 2, 1]
+
+def reverse(array):
+    
+  return array[::-1]
+
+print(reverse([1, 2, 3, 4, 5]))
