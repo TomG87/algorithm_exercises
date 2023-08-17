b1ffef8dc529b67dc79e98126c4e75ce43d8a807
Tom Gagliano <tgaglianoll@gmail.com>
2023-08-17 19:53:08 -0400

    solve reverse string problem

---
+++ string/reverse.py
@@ -0,0 +1,10 @@
+# Write a function that returns the reverse of a given string.
+
+# Input: “abcde”
+# Output: “edcba”
+
+def backwards(string):
+    
+  return string[::-1]
+
+print(backwards("abcde"))
