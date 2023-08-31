60ffaf8226993ee4e8333cb8f9b8588e27957c9f
Tom Gagliano <tgaglianoll@gmail.com>
2023-08-31 10:46:13 -0400

    solve fibonacci problem in javascript

---
+++ number/fibonacci.js
@@ -0,0 +1,23 @@
+// # Write a function that gives the Nth number of the Fibonacci Sequence. The Fibonacci sequence begins with 0 and 1, and every number thereafter is the sum of the previous two numbers. So the sequence goes like this:
+
+// # 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, and so on until infinity...
+
+// # Input: 9
+// # Output: 21 (as this is the 9th number of the Fibonacci Sequence)
+
+function fibonacci(number) {
+  let i = 0;
+  let i2 = 1;
+  let i3 = 2;
+
+  while (i < number) {
+    total = i + i2;
+    i = i2;
+    i2 = total;
+    i3 += 1;
+  }
+
+  return i2;
+}
+
+console.log(fibonacci(9));
