10e62cfd5e68d60d34cf30d5cd2fbef622cffee2
Tom Gagliano <tgaglianoll@gmail.com>
2023-09-01 16:51:43 -0400

    solve leap problem in python

--- number/leap.py
+++ number/leap.py
@@ -8,3 +8,11 @@
 # For example, 1997 is not a leap year, but 1996 is. 1900 is not a leap year, but 2000 is.
 
 # If your language provides a method in the standard library that does this look-up, pretend it doesn't exist and implement it yourself.
+
+def leap(number):
+  if (number % 4 == 0 and number % 100 != 0) or (number % 400 == 0):
+    return f'{number} is a leap year'
+  else:
+    return f'{number} is not a leap year'
+
+print(leap(2000)) 
