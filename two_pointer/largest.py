75c92ea462be82ca00c23123cf472f112e962c12
Tom Gagliano <tgaglianoll@gmail.com>
2023-09-12 10:49:46 -0400

    solve largest problem in python

--- two_pointer/largest.py
+++ two_pointer/largest.py
@@ -2,3 +2,21 @@
 
 # Input: [5, -2, 1, -9, -7, 2, 6]
 # Output: 63 (-9 * -7)
+
+def largest(array):
+
+  i = 0
+  i2 = 1
+  max = array[i] * array[i2]
+
+  while i2 < len(array):
+    total = array[i] * array[i2]
+    if total > max:
+       max = total
+    i += 1
+    i2 += 1
+
+  return max
+
+
+print(largest([5, -2, 1, -9, -7, 2, 6]))
