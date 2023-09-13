cae0119ebb58957ccbe791e99fd4aaaf4dafa438
Tom Gagliano <tgaglianoll@gmail.com>
2023-09-13 14:41:48 -0400

    solve twosum problem in python

--- two_pointer/twosum.py
+++ two_pointer/twosum.py
@@ -8,3 +8,18 @@
 # Input: [1, 2, 3, 4, 5]
 # Output: false (While 1, 2, 3, and 4 altogether add up to 10, we're seeking just one pair of numbers.)
 
+def twosum(array):
+
+  i = 0
+
+  while i < len(array):
+    for number in array:
+      total = array[i] + number
+      newArray = [array[i], number]
+      if array[i] != number and total == 10:
+          return newArray
+    i += 1
+
+  return False 
+
+print(twosum([2, 5, 3, 1, 0, 7, 11]))
