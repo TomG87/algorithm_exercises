da60eb17d583429399577f960d5663a80e4409f6
Tom Gagliano <tgaglianoll@gmail.com>
2023-09-18 12:16:01 -0400

    solve coolio array problem in python

---
+++ two_pointer/coolio.py
@@ -0,0 +1,29 @@
+# // # Given an array of numbers, return true if the array is a "100 Coolio Array", or false if it is not.
+
+# // # A "100 Coolio Array" meets the following criteria:
+
+# // # Its first and last numbers add up to 100,
+# // # Its second and second-to-last numbers add up to 100,
+# // # Its third and third-to-last numbers add up to 100,
+# // # and so on and so forth.
+
+# // # Here are examples of 100 Coolio Arrays:
+
+# // # [1, 2, 3, 97, 98, 99]
+# // # [90, 20, 70, 100, 30, 80, 10]
+
+def coolio(array):
+  i = 0
+  j = -1
+  length = len(array)//2
+
+  while i < length:
+    total = (array[i] + array[j])
+    if total != 100:
+      return False
+    i += 1
+    j -= 1
+
+  return "Coolio Array"
+
+print(coolio([1, 2, 3, 97, 98, 99]))
