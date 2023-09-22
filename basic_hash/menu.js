ff8de6405684e67a5e52fc8d41c752e8efc44916
Tom Gagliano <tgaglianoll@gmail.com>
2023-09-22 12:11:54 -0400

    solve menu problem in javascript

--- basic_hash/menu.js
+++ basic_hash/menu.js
@@ -5,3 +5,25 @@
 // Output: 14
 
 // Explanation: If someone would order one of everything on the menu, they'd pay a total of 14 (the sum of all the hash's values).
+
+function menu(menu_items) {
+  let total = 0;
+
+  for (const itemName in menu_items) {
+    const price = menu_items[itemName];
+    total += price;
+  }
+
+  return total;
+}
+
+const menu_items = {
+  "hot dog": 2,
+  hamburger: 3,
+  "steak sandwich": 5,
+  fries: 1,
+  "cole slaw": 1,
+  soda: 2,
+};
+
+console.log(menu(menu_items));
