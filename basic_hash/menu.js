7df6823a3d0d8593499b0fb34bbae2f7b4d62c13
Tom Gagliano <tgaglianoll@gmail.com>
2023-09-26 16:46:25 -0400

    solve posts problem in ruby

--- basic_hash/menu.js
+++ basic_hash/menu.js
@@ -6,18 +6,18 @@
 
 // Explanation: If someone would order one of everything on the menu, they'd pay a total of 14 (the sum of all the hash's values).
 
-function menu(menu_items) {
+function menu(menuItems) {
   let total = 0;
 
-  for (const itemName in menu_items) {
-    const price = menu_items[itemName];
+  for (const itemName in menuItems) {
+    const price = menuItems[itemName];
     total += price;
   }
 
   return total;
 }
 
-const menu_items = {
+const menuItems = {
   "hot dog": 2,
   hamburger: 3,
   "steak sandwich": 5,
@@ -26,4 +26,4 @@ const menu_items = {
   soda: 2,
 };
 
-console.log(menu(menu_items));
+console.log(menu(menuItems));
---
+++ basic_hash/posts.rb
@@ -0,0 +1,36 @@
+# Given an array of hashes that represent a list of social media posts, return a new array that only contains the posts that have at least 1000 likes.
+
+# Input: [
+# {title: 'Me Eating Pizza', submitted_by: "Joelle P.", likes: 1549},
+# {title: 'i never knew how cool i was until now', submitted_by: "Lyndon Johnson", likes: 3},
+# {title: 'best selfie evar!!!', submitted_by: "Patti Q.", likes: 1092},
+# {title: 'Mondays are the worst', submitted_by: "Aunty Em", likes: 644}
+# ]
+
+# Output: [
+# {title: 'Me Eating Pizza', submitted_by: "Joelle P.", likes: 1549},
+# {title: 'best selfie evar!!!', submitted_by: "Patti Q.", likes: 1092},
+# ]
+
+def posts(array)
+  
+  new_array = []
+  i = 0
+  
+  while i < array.length
+    if array[i][:likes] > 1000
+      new_array << array[i]
+    end
+    i += 1
+  end
+    return new_array
+end
+
+likes = [
+  {title: 'Me Eating Pizza', submitted_by: "Joelle P.", likes: 1549},
+  {title: 'i never knew how cool i was until now', submitted_by: "Lyndon Johnson", likes: 3},
+  {title: 'best selfie evar!!!', submitted_by: "Patti Q.", likes: 1092},
+  {title: 'Mondays are the worst', submitted_by: "Aunty Em", likes: 644}
+  ]
+
+p posts(likes)
