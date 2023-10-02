bbc6884c1142084b05c9c3aeca87f48606601aa8
Tom Gagliano <tgaglianoll@gmail.com>
2023-10-02 17:51:15 -0400

    solve anagram problem in javascript

--- basic_hash/anagram.js
+++ basic_hash/anagram.js
@@ -7,3 +7,21 @@
 
 // Input: “frog”, “bear”
 // Output: false
+
+function anagram(string, string2) {
+  let i = 0;
+
+  if (string.length === string2.length) {
+    while (i < string.length) {
+      if (string.includes(string2[i])) {
+        i += 1;
+      } else {
+        return false;
+      }
+      return true;
+    }
+  } else {
+    return false;
+  }
+}
+console.log(anagram("silent", "listen"));
